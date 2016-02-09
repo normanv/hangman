07135887198fbd7a754f669cb6825f338913cb7c
Norman Vourlat <norman@Normans-MacBook-Pro.local>
2016-02-10 09:52:33 +1300

    add hangman.rb first code

---
+++ hangman.rb
@@ -0,0 +1,11 @@
+
+#word entry
+puts "Hello game master !"
+print "Please give a word to guess for the player : "
+word = gets
+print "Thank you, now give the screen to the player =)"
+sleep 1
+system "clear"
+print "Welcome to the hangman game, you have to guess the word chosen by the master."
+
+print "Word :"
