0fdf890584cf68f2457ef96dcfc998168aa37ae6
Norman Vourlat <norman@Normans-MacBook-Pro.local>
2016-02-12 11:28:16 +1300

    separating game object from game logic

--- launch_hangman_game.rb
+++ launch_hangman_game.rb
@@ -1,11 +1,28 @@
-puts "Hello game master !"
-print "Please give a word to guess for the player : "
+require './hangman'
+
+def prepare(game)
+  puts "Hello game master !"
+  print "Please give a word to guess for the player : "
+  game.initialize_word("")
+  print "Thank you, now give the screen to the player =)"
+  game
+end
+
+def start(game)
+  puts "Welcome to the hangman game, you have to guess the word chosen by the master."
+  game.print_game_status
+  while game.running? do
+    guess = game.read_guess
+    game.analyze_guess(guess)
+    game.print_game_status
+  end
+  game.won?
+end
+
+#game = Hangman.new(6)
 game = Hangman.new
-game.initialize_word("test")
-print "Thank you, now give the screen to the player =)"
-sleep 1
+
+prepare(game)
 system "clear"
-puts "Welcome to the hangman game, you have to guess the word chosen by the master."
-result = game.start
-#this also work : puts result ? "You win" : "You lose"
+result = start(game)
 result ? puts("You win !") : puts("You lose !")
